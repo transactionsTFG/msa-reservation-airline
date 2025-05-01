25cd40668b914b4ee6af61c0ab8d6f4bf40b6146
danielfdez17 <daniel17dev@gmail.com>
2025-05-01 17:50:58 +0200

    feat: removed unused imports

--- src/main/java/business/reservationline/ReservationLine.java
+++ src/main/java/business/reservationline/ReservationLine.java
@@ -4,14 +4,10 @@ package business.reservationline;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-
-import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
